f53085ea2400fcb7a12c9d44ab891a60b0d022c3
Eric Garcia <egarcia1868@gmail.com>
2020-02-19 21:13:32 -0600

    markdown generator almost completed.  Needs touching up

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,49 @@
 function generateMarkdown(data) {
   return `
-# ${data.title}
+# ${data.projectName}
 
-`;
+## Description 
+
+${data.description}
+
+## Installation
+
+${data.depCom}
+
+## Usage 
+
+${data.needToKnow}
+
+## Credits
+
+${data.github}
+----------------List your collaborators, if any, with links to their GitHub profiles.
+
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+If you followed tutorials, include links to those here as well.
+
+
+
+## License
+
+${data.license}  vvprobably need to do something with this down there.
+
+---
+
+
+---------## Badges
+
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+## Contributing
+
+${data.contribute}
+
+## Tests
+
+${data.testCom}
+
 }
 
 module.exports = generateMarkdown;
